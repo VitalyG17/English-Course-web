5edc576a8eaaef13eb1e8eb920a16027aae2f2f9
VitalyG17 <VitalyG-17@yandex.ru>
2025-05-25 01:30:05 +0300

    В сайдбаре отображаю данные пользователя

--- src/app/sidebar-menu/sidebar-menu.component.ts
+++ src/app/sidebar-menu/sidebar-menu.component.ts
@@ -1,5 +1,5 @@
 import {AsyncPipe, NgIf} from '@angular/common';
-import {ChangeDetectionStrategy, Component, inject, signal, WritableSignal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, Signal, signal, WritableSignal} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {TuiThemeColorService} from '@taiga-ui/cdk';
 import {TuiButton, TuiDataList, TuiDropdown, TuiFallbackSrcPipe} from '@taiga-ui/core';
@@ -14,6 +14,10 @@ import {
 } from '@taiga-ui/kit';
 import {TuiNavigation} from '@taiga-ui/layout';
 import {RouterModule} from '@angular/router';
+import {ProfileService} from '../profile/shared/services/profile.service';
+import {Profile} from '../profile/shared/models/profile.model';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'sidebar-menu',
@@ -42,6 +46,17 @@ import {RouterModule} from '@angular/router';
 })
 export class SidebarMenuComponent {
   private readonly theme: TuiThemeColorService = inject(TuiThemeColorService);
+  private readonly profileService: ProfileService = inject(ProfileService);
+
+  protected readonly profileInfo: Signal<Profile> = toSignal(
+    this.profileService.getProfile().pipe(
+      map((profile: Profile) => {
+        profile.avatarUrl = `http://localhost:3000/uploads/${profile.avatarUrl}`;
+        return profile;
+      }),
+    ),
+    {initialValue: new Profile()},
+  );
 
   protected expanded: WritableSignal<boolean> = signal(true);
   protected open: boolean = false;
