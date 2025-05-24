ccaaf2c49b74e5bef8056dfd389c0bce317f4c3e
VitalyG17 <VitalyG-17@yandex.ru>
2025-05-25 01:18:45 +0300

    Изменил на странице профиля отображение блока "Пройденные курсы", чтобы в нем отображались реальные данные пользователя

--- src/app/profile/courses-list/courses-list.component.ts
+++ src/app/profile/courses-list/courses-list.component.ts
@@ -1,14 +1,48 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {TuiAppearance, TuiTitle} from '@taiga-ui/core';
+import {TuiAppearance, TuiButton, TuiTitle} from '@taiga-ui/core';
 import {TuiCardLarge, TuiCardMedium, TuiHeader} from '@taiga-ui/layout';
-import {TuiRepeatTimes} from '@taiga-ui/cdk';
+import {inject, Signal} from '@angular/core';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {map} from 'rxjs/operators';
+import {RouterLink} from '@angular/router';
+import {CoursesService} from '../../courses/courses-page/shared/services/courses.service';
+import {Courses} from '../../courses/courses-page/shared/models/courses.model';
+import {NgForOf, NgIf} from '@angular/common';
+import {TuiAvatar} from '@taiga-ui/kit';
 
 @Component({
   selector: 'courses-list',
   standalone: true,
   templateUrl: './courses-list.component.html',
   styleUrl: './courses-list.component.scss',
-  imports: [TuiAppearance, TuiCardLarge, TuiCardMedium, TuiHeader, TuiRepeatTimes, TuiTitle],
+  imports: [
+    TuiAppearance,
+    TuiCardLarge,
+    TuiCardMedium,
+    TuiHeader,
+    TuiTitle,
+    NgIf,
+    NgForOf,
+    TuiAvatar,
+    TuiButton,
+    RouterLink,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CoursesListComponent {}
+export class CoursesListComponent {
+  private readonly coursesService: CoursesService = inject(CoursesService);
+
+  protected readonly completedCourses: Signal<Courses[]> = toSignal(
+    this.coursesService.getAllWithProgress().pipe(
+      map((courses: Courses[]) =>
+        courses
+          .filter((course: Courses) => course.isCompleted)
+          .map((course: Courses) => ({
+            ...course,
+            imageUrl: `http://localhost:3000/uploads/courses/${course.imageUrl}`,
+          })),
+      ),
+    ),
+    {initialValue: []},
+  );
+}
